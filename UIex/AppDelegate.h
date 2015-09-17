76d1c63effd2b36fde58b769a452de0f12d92019
WJXXT <495820035@qq.com>
2015-09-17 19:18:34 +0800

    Initial Commit

---
+++ UIex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIex
+//
+//  Created by XXT on 15/9/17.
+//  Copyright (c) 2015年 XXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIex
+//
+//  Created by XXT on 15/9/17.
+//  Copyright (c) 2015年 XXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
